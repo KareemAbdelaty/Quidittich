f6ede4dc87b68a5aba201ef1f2b79ddb394397fe
KareemAbdelaty <69083495+KareemAbdelaty@users.noreply.github.com>
2021-02-26 06:01:02 +0200

    Created Wizard Behaviour

--- Assets/SnitchBehaviourScript.cs
+++ Assets/SnitchBehaviourScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class SnitchBehaviourScript : MonoBehaviour
 {
+    public string id = "Snitch";
+    public int x = 10;
+    public int y = 10;
+    public int z = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class SnitchBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + new Vector3(x* Time.deltaTime, x* Time.deltaTime, z * Time.deltaTime);
+    }
+    void FixedUpdate() { 
+    
+    }
+    public void generateNewCoordinates()
+    {
+
+    }
+    void LateUpdate()
+    {
+
     }
 }
